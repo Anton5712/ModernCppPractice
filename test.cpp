c8cef8d9809289bd72158e5c899b51b99afebea7
Anton5712 <anton5712@gmail.com>
2023-08-08 16:08:38 +0300

    Add stringstream

--- test.cpp
+++ test.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 int main(int argc, char* argv[])
 {
     std::string helloWorldSTr = "Hello world from feature";
     std::cout  << helloWorldSTr << std::endl;
+
+    std::stringstream ss;
+    ss << "One " << "2 " << 3;
+    std::cout << ss.str() << std::endl;
     return 0;
 }
