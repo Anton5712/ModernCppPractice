e398695f42654a88c474e33e6cc9df50b738cab7
Anton5712 <anton5712@gmail.com>
2023-08-08 15:49:42 +0300

    Add string
    - change str hello world

--- test.cpp
+++ test.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
-    std::cout  << "Hello WOrld!!!" << std::endl;
+    std::string helloWorldSTr = "Hello world from feature";
+    std::cout  << helloWorldSTr << std::endl;
     return 0;
 }
