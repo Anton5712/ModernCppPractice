218afd888a14f23991dd78c755363109f0396945
Anton5712 <anton5712@gmail.com>
2023-08-09 21:00:13 +0300

    Add
    log

--- test.cpp
+++ test.cpp
@@ -30,13 +30,17 @@ public:
     template <typename U>
     T add(U x) {
         if constexpr ( std::is_same_v<T, std::vector<U>> ) {
-                    auto copy(_value);
-        for (auto& n : copy ) {
-            n += x;
-        }
-        return copy;
+            auto copy(_value);
+            for (auto& n : copy ) {
+                n += x;
+            }
+            std::cout << "Vector: " << std::endl;
+            for (const auto& n : copy)
+                std::cout << n << std::endl;
+            return copy;
         }
         else {
+            std::cout << "Value: " << (_value + x) << std::endl;
             return _value + x;
         }
     }
@@ -46,5 +50,6 @@ public:
 int main(int argc, char* argv[])
 {
     addable<int>{1}.add(2);
+    addable<std::vector<int>>{ std::vector<int> {1,2,3} }.add(10);
     return 0;
 }
